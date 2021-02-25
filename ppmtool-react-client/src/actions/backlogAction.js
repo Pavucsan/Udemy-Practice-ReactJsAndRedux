1fbbc87e5c3c9a99f45d5b5878ecbcf6c6401c16
pavucsan <pavucsan.pro@gmail.com>
2021-02-26 01:14:11 +0530

    ppmtool ready to deploy 1

--- ppmtool-react-client/src/actions/backlogAction.js
+++ ppmtool-react-client/src/actions/backlogAction.js
@@ -81,7 +81,7 @@ export const updateProjectTask = (
   } catch (error) {
     dispatch({
       type: GET_ERRORS,
-      payload: error.response,
+      payload: error.response.data,
     });
   }
 };
